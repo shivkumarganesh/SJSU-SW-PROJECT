9c24a8761c1d69cd8771ecd73ece1b8756d8c7bb
Shiv Kumar Ganesh <GShiv.sk@gmail.com>
2021-04-24 01:30:55 -0700

    Initial commit - Updated Spacing

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing(1),
-    marginLeft: theme.spacing(10)
+    marginLeft: theme.spacing(9)
   }
 }));
 
