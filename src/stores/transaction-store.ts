028c9b758c9a0b7bb57647246d350b8caf2d2964
Shiv Kumar Ganesh <GShiv.sk@gmail.com>
2021-04-29 15:17:00 -0700

    Updated Config

--- src/stores/transaction-store.ts
+++ src/stores/transaction-store.ts
@@ -5,20 +5,15 @@ import { Transactions } from "./../models/transactions";
 import { transactions } from "./../mock-data/transactions";
 
 export class TransactionStore {
-  @observable transactions: Transactions[] = [] as Transactions[];
+  @observable transactions: Transactions[] = [];
 
   constructor() {
     const transactions = this.fetchTransactions();
     this.setTransactions(transactions);
   }
 
-  fetchTransactions(id?: string) {
-    // Fetch Transactions from the backend by ID or All if ID not present
-    if (id) {
-      return [transactions[0] as Transactions];
-    } else {
-      return transactions as Transactions[];
-    }
+  fetchTransactions() {
+    return transactions;
   }
 
   get allTransactions() {
